17a7fb0fa75634e7cbb5b73277d3f1b725f79a8a
Berendea Andrei <berendeaandrei03@gmail.com>
2024-02-29 14:56:21 +0200

    am adaugat o citire de la tastatura

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import pachet.Help;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Help help=new Help();
-        help.see();
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.next();
+        if (s=="hello") help.see();
+        else System.out.println("nu e hello");
     }
 }
